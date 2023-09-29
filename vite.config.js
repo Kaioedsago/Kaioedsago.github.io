d25b1511f89906df2df0873c33a03c97db200828
Kaioeduduardo <kaioedsago@gmail.com>
2023-09-29 10:44:46 -0300

    please go

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/solardeploy",
+  base: "/",
+  // server: {
+  //   port: 3000,
+  //   host: true
+  // },
   plugins: [react()],
 })
