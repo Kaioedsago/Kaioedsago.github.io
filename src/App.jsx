8d4e08d92ad3172a78897fe066c93df0d9726a30
Kaioedsago <Kaioedsago@gmail.com>
2023-09-30 13:53:26 -0300

    Feat: Routes now is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-const App = () =>{
+import Provider from "./context/provider";
+import { Routes, Route } from "react-router-dom";
+import './index.css';
+import { pages } from "./routes/Rotas";
 
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div>
+        <Provider>
+        <Routes>
+          {pages.map((page, index) => {
+            return (
+              <Route
+                key={index + "-route"}
+                path={page.path}
+                element={<page.component />}
+              />
+            );
+          })}
+        </Routes>
+      </Provider>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/routes/Rotas.jsx
@@ -0,0 +1,10 @@
+import Home from "../pages/Home";
+
+
+export const pages = [
+  {
+    path: "/",
+    component: Home,
+    title: "Portfólio",
+  }
+];
